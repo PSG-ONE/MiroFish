c31bbaa6d3827fa5488c5fd8ba62705ccc9622dc
Pablo Santa Gadea <santagadea.ia@gmail.com>
2026-04-26 00:56:40 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,9 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: true,
+    open: false, 
+    allowedHosts: true, 
+    host: '0.0.0.0', 
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
