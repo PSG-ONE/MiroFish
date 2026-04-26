1dcd15abbf2864e855c6a7ae3869e5ca1e63e2aa
Pablo Santa Gadea <santagadea.ia@gmail.com>
2026-04-26 01:00:08 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,9 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    open: true,
+    open: false, 
+    allowedHosts: ['uptime-ai-swarm.3iaeor.easypanel.host'], 
+    host: '0.0.0.0', 
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
